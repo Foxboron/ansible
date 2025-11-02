16fadd8a0212e6283a1db7485e2f23153403ac18
Morten Linderud <morten@linderud.pw>
2025-11-02 15:06:06 +0100

    incus: add project module
    Signed-off-by: Morten Linderud <morten@linderud.pw>

---
+++ terraform/incus/modules/project/main.tf
@@ -0,0 +1,46 @@
+resource "incus_project" "project" {
+  name        = "${var.name}"
+  description = "${var.name} project"
+  config = {
+    "features.images"                = "true"
+    "features.networks"              = "false"
+    "features.networks.zones"        = "true"
+    "features.profiles"              = "true"
+    "features.storage.volumes"       = "true"
+    "features.storage.buckets"       = "true"
+    "restricted"                     = "true"
+    "restricted.containers.nesting"  = "allow"
+    "restricted.devices.disk"        = "allow"
+    "restricted.devices.gpu"         = "allow"
+    "restricted.devices.proxy"       = "allow"
+    "restricted.containers.lowlevel" = "allow"
+    "restricted.snapshots"           = "allow"
+    "restricted.devices.nic"         = "allow"
+    "restricted.networks.access"     = "br0"
+  }
+}
+
+resource "incus_profile" "default" {
+  name = "default"
+  description = "Default Incus profile for project ${var.name}"
+  project = incus_project.project.name
+
+  device {
+    name = "eth0"
+    properties = {
+      "nictype" = "bridged"
+      "parent"  = "br0"
+    }
+    type = "nic"
+  }
+
+  device {
+    type = "disk"
+    name = "root"
+    properties = {
+      pool = "default"
+      path = "/"
+    }
+  }
+}
+
---
+++ terraform/incus/modules/project/outputs.tf
@@ -0,0 +1,3 @@
+output "name" {
+	value = incus_project.project.name
+}
