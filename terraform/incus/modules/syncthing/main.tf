c028a0efcabd55fb23f4aa6dcaab7c1e38dd001a
Morten Linderud <morten@linderud.pw>
2025-11-15 00:43:40 +0100

    syncthing: add hackeriet node
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- terraform/incus/modules/syncthing/main.tf
+++ terraform/incus/modules/syncthing/main.tf
@@ -108,3 +108,76 @@ resource "incus_instance" "syncthing" {
     }
   }
 }
+
+resource "incus_instance" "syncthing_hackeriet" {
+  name    = "syncthing-hackeriet"
+  image   = incus_image.syncthing.fingerprint
+  project = module.syncthing_project.name
+  target = "hackeriet"
+
+  config = {
+    "environment.PUID" =  "1001"
+    "environment.PGID" =  "1001"
+    "environment.TZ"   =  "Europe/Oslo"
+  }
+
+  device {
+    # Port 8384 forward
+    name = "port-tcp-8384"
+    type = "proxy"
+    properties = {
+      listen = "tcp:0.0.0.0:8384"
+      connect = "tcp:127.0.0.1:8384"
+    }
+  }
+
+  device {
+    # Port 22000 forward
+    name = "port-tcp-22000"
+    type = "proxy"
+    properties = {
+      listen = "tcp:0.0.0.0:22000"
+      connect = "tcp:127.0.0.1:22000"
+    }
+  }
+
+  device {
+    # Port 22000 forward
+    name = "port-udp-22000"
+    type = "proxy"
+    properties = {
+      listen = "udp:0.0.0.0:22000"
+      connect = "udp:127.0.0.1:22000"
+    }
+  }
+
+  device {
+    # Port 22000 forward
+    name = "port-udp-21027"
+    type = "proxy"
+    properties = {
+      listen = "udp:0.0.0.0:21027"
+      connect = "udp:127.0.0.1:21027"
+    }
+  }
+
+  device {
+    # Config mount
+    name = "config"
+    type = "disk"
+    properties = {
+      source = "/var/syncthing/config/"
+      path = "/config/"
+    }
+  }
+
+  device {
+    # data1 mount
+    name = "syncthing-srv"
+    type = "disk"
+    properties = {
+      source = "/srv/"
+      path = "/srv/"
+    }
+  }
+}
